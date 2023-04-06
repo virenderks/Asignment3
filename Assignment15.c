e3e9bb77d9a083b1a291731727aa1d5fb520b97c
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-04-06 13:14:10 +0530

    Add files via upload

---
+++ Assignment15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    // Take input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    // Check if the number is positive, negative, or zero
+    if(num > 0) {
+        printf("%d is positive.\n", num);
+    } else if(num < 0) {
+        printf("%d is negative.\n", num);
+    } else {
+        printf("The number is zero.\n");
+    }
+    
+    return 0;
+}
